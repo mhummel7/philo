26a27bd7c209c4137a8bee1889ef7409282ad721
Manuel <mhummel@student.42heilbronn.de>
2024-11-04 12:30:39 +0100

    Update create_threads
    Added a small delay between thread creations to avoid thundering herd

--- src/threads.c
+++ src/threads.c
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:51:53 by mhummel           #+#    #+#             */
-/*   Updated: 2024/10/31 13:02:46 by mhummel          ###   ########.fr       */
+/*   Updated: 2024/11/04 12:30:24 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,13 @@ int	create_threads(t_data *data)
 	i = -1;
 	while (++i < data->philo_num)
 	{
-		data->philos[i].time_to_die = data->start_time + data->death_time;
-		if (pthread_create(&data->philo_thread_id[i], NULL,
-				philosopher_routine, &data->philos[i]) != 0)
+		pthread_mutex_lock(&data->philos[i].lock);
+		data->philos[i].time_to_die = get_time() + data->death_time;
+		pthread_mutex_unlock(&data->philos[i].lock);
+		if (pthread_create(&data->philo_thread_id[i], NULL, philosopher_routine,
+				&data->philos[i]) != 0)
 			ft_exit("Thread creation failed", data);
+		usleep(100);
 	}
 	return (0);
 }
