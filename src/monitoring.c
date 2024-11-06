e65c0ec3399d829816f5ad3f39c2e402ee3a2d95
Manuel <mhummel@student.42heilbronn.de>
2024-11-06 10:56:15 +0100

    Update check_death
    Split the function because I had 30 lines. death checking logic is in seperate static function i created named check_philo_death. Functionality is the same as before

--- src/monitoring.c
+++ src/monitoring.c
@@ -6,37 +6,42 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:43:15 by mhummel           #+#    #+#             */
-/*   Updated: 2024/11/06 09:56:04 by mhummel          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:50:45 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	check_philo_death(t_data *data, int i)
+{
+	uint64_t	current_time;
+
+	pthread_mutex_lock(&data->philos[i].lock);
+	current_time = get_time();
+	if (!data->philos[i].eating && current_time > data->philos[i].time_to_die)
+	{
+		print_status(&data->philos[i], "died");
+		pthread_mutex_lock(&data->lock);
+		data->dead = 1;
+		pthread_mutex_unlock(&data->lock);
+		pthread_mutex_unlock(&data->philos[i].lock);
+		return (1);
+	}
+	pthread_mutex_unlock(&data->philos[i].lock);
+	return (0);
+}
+
 void	check_death(t_data *data)
 {
 	int			i;
-	uint64_t	current_time;
 	int			should_stop;
 
 	while (!data->finished && !data->dead)
 	{
 		i = -1;
 		while (++i < data->philo_num)
-		{
-			pthread_mutex_lock(&data->philos[i].lock);
-			current_time = get_time();
-			if (!data->philos[i].eating
-				&& current_time > data->philos[i].time_to_die)
-			{
-				print_status(&data->philos[i], "died");
-				pthread_mutex_lock(&data->lock);
-				data->dead = 1;
-				pthread_mutex_unlock(&data->lock);
-				pthread_mutex_unlock(&data->philos[i].lock);
+			if (check_philo_death(data, i))
 				return ;
-			}
-			pthread_mutex_unlock(&data->philos[i].lock);
-		}
 		pthread_mutex_lock(&data->lock);
 		should_stop = data->dead || data->finished >= data->philo_num;
 		pthread_mutex_unlock(&data->lock);
